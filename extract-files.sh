6d1ccc30e9ae08eebd1d7fae9221456a42e31071
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-07 17:52:14 +0800

    device.mk: Use libcrypto-v33 for widevine libs
    * The symbol `CBS_init` is no longer exported from latest libcrypto
    * libcrypto from VNDK 33 is available in hardware/lineage/compat,
      and we had no problem with it on VNDK 33. Let's use it.
    
    Change-Id: I6e9b0857df4650e8d5d63793716fe84da93ab165
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- extract-files.sh
+++ extract-files.sh
@@ -71,8 +71,14 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
-	vendor/etc/media_codecs.xml|vendor/etc/media_codecs_lahaina.xml|vendor/etc/media_codecs_lahaina_vendor.xml|vendor/etc/media_codecs_yupik_v1.xml)
+	    vendor/etc/media_codecs.xml|vendor/etc/media_codecs_lahaina.xml|vendor/etc/media_codecs_lahaina_vendor.xml|vendor/etc/media_codecs_yupik_v1.xml)
             sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
+            ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+            ;;
+        *)
+            return 1
             ;;
     esac
 }
