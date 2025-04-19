2873c1e79c94aa14ed78af96c39544702c69d1fa
DaViDev985 <davidev985@gmail.com>
2025-04-19 18:41:03 +0200

    configs: Import displayconfig from previous trees

--- device.mk
+++ device.mk
@@ -186,6 +186,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.memtrack-service
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946480328692354.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946480328692354.xml
 
 # DRM
 PRODUCT_PACKAGES += \
