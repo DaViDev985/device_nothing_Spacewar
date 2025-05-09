30462a7584b18ac42f66feb5c0c206bf2a7db68a
DaViDev985 <davidev985@gmail.com>
2025-05-09 18:52:41 +0200

    device.mk: Add DerpFest flags

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 # NT Camera
 $(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
 
+# DerpFest Flags
+DERPFEST_BUILD_TYPE := Official
+DERP_VERSION_APPEND_TIME_OF_DAY=true
+TARGET_NOT_USES_BLUR=true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
