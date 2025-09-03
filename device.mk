3539690d69cd213bc23160c0a19da809dcd87d13
DaViDev985 <davidev985@gmail.com>
2025-09-03 18:45:39 +0200

    device.mk: Re-include Nothing Camera A fix is coming soon for video functionality.

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # NT Camera
-#$(call inherit-product, vendor/nothing/camera/nothing-camera.mk) commented out as Android 16 breaks it
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
 
 # DerpFest Flags
 DERPFEST_BUILD_TYPE := Official
